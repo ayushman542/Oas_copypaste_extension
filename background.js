4c3176c0d7f649f408df9f41c8101bb86b7a37f9
Ayushman Medcalia <118107461+ayushman542@users.noreply.github.com>
2024-07-29 06:05:56 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.runtime.onInstalled.addListener((details) => {
     if (details.reason === "install") {
-        chrome.tabs.create({ url: 'https://t.me/helpinghandlpu' });
+        chrome.tabs.create({ url: 'https://t.me/hackscript900' });
     }
 });
 console.log("Developer By Ayushman Medcalia , Follow me on telegram https://t.me/helpinghandlpu , follow me on Github https://github.com/ayushman542 , Follow me on linkedin https://www.linkedin.com/in/ayushman-medcalia-4a0908252/ ");
